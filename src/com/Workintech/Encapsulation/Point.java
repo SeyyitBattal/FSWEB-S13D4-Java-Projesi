07a5effeb7004f718a0aae2edf647643900c8da7
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-09-21 13:51:13 +0300

    Point class created

---
+++ src/com/Workintech/Encapsulation/Point.java
@@ -0,0 +1,34 @@
+package com.Workintech.Encapsulation;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+
+    }
+
+    public double distance() {
+        return Math.sqrt(0);
+
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+}
